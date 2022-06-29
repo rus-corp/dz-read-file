f1b839a53f26c5c5e4f6e0c5c31e4e291a8bc541
Руслан Прусаков <ruslan.prusakov@gmail.com>
2022-06-29 10:35:04 +0300

    full

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
-import os
-
-# file_name = 'data.txt'
+file_name = 'data.txt'
 
       
-# def file_reader(file_name):
-#     with open(file_name, encoding='utf-8') as file:
-#         cook_book = {}
-#         for line in file:
-#             line = line.strip()
-#             cook_book.update({line : []})
-#             for item in range(int(file.readline().strip())):
-#                 lst = file.readline().strip().split('|')
-#                 dict = {'name':lst[0], 'quan':lst[1], 'mes': lst[2]}
-#                 cook_book[line].append(dict)
-#             file.readline()
-#         return cook_book
+def file_reader(file_name):
+    with open(file_name, encoding='utf-8') as file:
+        cook_book = {}
+        for line in file:
+            line = line.strip()
+            cook_book.update({line : []})
+            for item in range(int(file.readline().strip())):
+                lst = file.readline().strip().split('|')
+                dict = {'name':lst[0], 'quan':lst[1], 'mes': lst[2]}
+                cook_book[line].append(dict)
+            file.readline()
+        return cook_book
 
-# print(file_reader(file_name))
+print(file_reader(file_name))
 
-# cook_book = file_reader(file_name)
+cook_book = file_reader(file_name)
 
 
-# def get_shop_list_by_dishes(dishes, person_count):
-#   menu = {}
-#   for k, v in cook_book.items():
-#     if dishes == k:
-#       for item in v:
-#         menu1 = item.copy()
-#         menu1['quan'] = menu1.get('quan') * person_count
-#         x = menu1.get('name')
-#         menu[x] = menu1
-#   return (menu)
+def get_shop_list_by_dishes(dishes, person_count):
+  menu = {}
+  for k, v in cook_book.items():
+    if dishes == k:
+      for item in v:
+        menu1 = item.copy()
+        menu1['quan'] = menu1.get('quan') * person_count
+        x = menu1.get('name')
+        menu[x] = menu1
+  return (menu)
 
-# print(get_shop_list_by_dishes('Омлет', 3))
+print(get_shop_list_by_dishes('Омлет', 3))
 
-file1 = '1.txt'
-file2 = '2.txt'
-file3 = '3.txt'
-file4 = 'tot_file.txt'
-
-list_of_files = os.listdir('dir_with_files')
-print(list_of_files)
 
 
 
